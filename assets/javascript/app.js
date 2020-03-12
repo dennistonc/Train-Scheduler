e4fcf258d50984de119a33714fa9aa64aa8dd704
dennistonc <dennistonc@gmail.com>
2020-03-11 23:51:53 -0500

    html changes and firebase added to js

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAl8tSTTwC3FIPamosaXCO9tUFiCgHubd4",
+    authDomain: "employees-2aa4a.firebaseapp.com",
+    databaseURL: "https://employees-2aa4a.firebaseio.com",
+    projectId: "employees-2aa4a",
+    storageBucket: "employees-2aa4a.appspot.com",
+    messagingSenderId: "846406660678",
+    appId: "1:846406660678:web:05d5dddcf887320955d047"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
